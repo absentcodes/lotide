0070eb140cab7ea4779a5a073815da064ca7f99a
absentcodes <absentpassengercodes@gmail.com>
2023-11-17 05:35:23 +0300

    added additional assertion test cases

--- tail.js
+++ tail.js
@@ -20,6 +20,19 @@ assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
+// Test Case 2: Check the returned array for one element
+const resultTwo = tail(["Hello"]);
+assertEqual(resultTwo.length, 0); 
+assertEqual(resultTwo[0], undefined); 
+
+
+// Test Case 3: Check the returned array for an empty array
+const resultThree = tail([]);
+assertEqual(resultThree.length, undefined); 
+assertEqual(resultThree[0], undefined); 
+
+
+
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
