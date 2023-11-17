4fc94d0c9087414bdeb32eb544e7232254dd93bc
absentcodes <absentpassengercodes@gmail.com>
2023-11-17 04:28:24 +0300

    implement head

---
+++ head.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const head = function(arr) {
+  return arr[0];
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([5,6,7]), 6);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+assertEqual(head(["Palestine"]), "Palestine");
+assertEqual(head([""]), undefined);
