28efaaf64a5e651a5d2152b1e4510d9baeefc623
absentcodes <absentpassengercodes@gmail.com>
2023-11-23 01:08:14 +0300

    implemented a countLetter function

---
+++ countLetters.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+ 
+const countLetters = function(string) {
+  const result = {};
+  for (let letter of string) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("L H L"));
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("   "));
+
+console.log(assertEqual((countLetters("LHL"))["L"], 2));
+console.log(assertEqual((countLetters("LHL"))["H"], 1));
