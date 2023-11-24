8bbeedff189348786634e25ce4680a8aaf01e5fc
absentcodes <absentpassengercodes@gmail.com>
2023-11-25 02:25:30 +0300

    implemented findKey function

---
+++ findKey.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+
+};
+
+const restaurants = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+};
+
+const results1 = findKey(restaurants, x => x.stars === 2);
+assertEqual(results1, "noma");
+
+const results2 = findKey(restaurants, x => x.stars === 3);
+assertEqual(results2, "Akaleri");
+
+const results3 = findKey(restaurants, x => x.stars === 5);
+assertEqual(results3, undefined);
