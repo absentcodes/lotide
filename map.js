deae5311c13d47a86b66802b06d301411ef1610d
absentcodes <absentpassengercodes@gmail.com>
2023-11-25 01:48:04 +0300

    implemented map function

---
+++ map.js
@@ -0,0 +1,47 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+};
+
+//Tests
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+//AssertionTests
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  const arraysAreEqual = eqArrays(arr1, arr2);
+
+  if (arraysAreEqual) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} != ${arr2}`);
+    
+  }
+};
+
+//Test Cases
+
+assertArraysEqual([ 'g', 'c', 't', 'm', 't' ], [ 'g', 'c', 't', 'm', 't' ]);
+
+
