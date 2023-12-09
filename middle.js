2971392bffef784cd4f68b307cc60c68445c5360
absentcodes <absentpassengercodes@gmail.com>
2023-12-09 21:40:20 +0300

    refactored using module export

--- middle.js
+++ middle.js
@@ -1,29 +1,5 @@
-// Test Assert Functions
-const assertArraysEqual = function(arr1, arr2) {
-  const arraysAreEqual = eqArrays(arr1, arr2);
-
-  if (arraysAreEqual) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} != ${arr2}`);
-    
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 // Middle Function
 const middle = function(array) {
@@ -42,11 +18,4 @@ const middle = function(array) {
 
 };
 
-//Test Codes
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
+module.exports = middle;
