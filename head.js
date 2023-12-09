af0c578ba10bb80583e022e89544427afea30f7c
absentcodes <absentpassengercodes@gmail.com>
2023-12-09 04:31:15 +0300

    refactored code using module exports

--- head.js
+++ head.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const head = function(arr) {
   if (Array.isArray(arr) && arr.length > 0) {
     return arr[0];
@@ -14,8 +5,4 @@ const head = function(arr) {
   return undefined;
 };
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["Palestine"]), "Palestine");
-assertEqual(head([""]));
+module.exports = head;
