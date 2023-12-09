5bb9eb79434394d43bf01f1a098254c2ac97dfc6
absentcodes <absentpassengercodes@gmail.com>
2023-12-09 22:12:22 +0300

    corrected test requirement

--- test/headTest.js
+++ test/headTest.js
@@ -6,8 +6,8 @@ describe("head", () => {
     assert.strictEqual(head([1, 2, 3]), 1)
   });
 
-  it("returns 5 for [5, 6, 7]", () => {
-    assert.strictEqual(head([5, 6, 7]), 5)
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5)
   });
 });
 
