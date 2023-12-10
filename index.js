ef2b1ec5ffe33d4b70caffeda8232c3a5514f368
absentcodes <absentpassengercodes@gmail.com>
2023-12-10 22:36:15 +0300

    corrected typo in function

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
-const map = reuqire('./map');
+const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
