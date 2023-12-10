d6e733a597a76bab5b7f77233e9980ef847cfe5b
absentcodes <absentpassengercodes@gmail.com>
2023-12-10 05:03:23 +0300

    added mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,26 +1,8 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: Check the returned array for one element
-const resultTwo = tail(["Hello"]);
-assertEqual(resultTwo.length, 0); 
-assertEqual(resultTwo[0], undefined); 
-
-
-// Test Case 3: Check the returned array for an empty array
-const resultThree = tail([]);
-assertEqual(resultThree.length, 0); 
-assertEqual(resultThree[0], undefined); 
-
-
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse','Labs']);
+  });
+});
